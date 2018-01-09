309da4c4903fbe6476c2bb22255fd4ee50d8f51b
matt6390 <matthewstone639@gmail.com>
2018-01-08 19:17:46 -0600

    Saving my work

--- data_munging.rb
+++ data_munging.rb
@@ -1,24 +1,39 @@
 input = File.open('./weather.dat', File::RDONLY){|f| f.read }
-array = input.lines.map(&:split)
+array = input.lines.map(&:split)     #reads the weather.dat file that was provided
 
 loops = array.length 
 temp_max = 0 
 temp_min = 0 
 day = 2 
 temp_spread = 0 
+winner = 100
+win_day = 0
 
 
 (loops - 3).times do 
   temp_min = array[day][2]
-  temp_max = array[day][1]
-  # temp_spread = (temp_max - temp_min)   #why is this line not working?
-
-
-  day += 1
-  p temp_max
+  temp_min = temp_min.to_i    #changes the strings into integers
   p temp_min
+  temp_max = array[day][1]
+  temp_max = temp_max.to_i
+  p temp_max
+  temp_spread = (temp_max - temp_min)   #why is this line not working? (temp_min and temp_max were strings for some reason)
+  day += 1
+  
+  if temp_spread < winner 
+    winner = temp_spread
+    win_day = day
+
+  end
+
+
+
+  # p temp_max
+  # p temp_min
   p temp_spread
 end 
+
+p win_day
 
 
 
